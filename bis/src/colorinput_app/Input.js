043da4850dd322e622b965aa391130facd33b4a9
jmabbas <mdabbas.it@gmail.com>
2025-02-18 21:49:17 +0530

    color input app

---
+++ bis/src/colorinput_app/Input.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import colorNames from 'colornames'
+
+const Input = ({colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+        <label> Add Color Names</label>
+        <input
+            autoFocus
+            type='text'
+            placeholder='add color name'
+            required
+            value={colorValue}
+            onChange={ (e) => {
+                setColorValue(e.target.value);
+                setHexValue(colorNames(e.target.value));
+            }}
+        />
+        <button type='button'
+            onClick={()=> setIsDarkText(!isDarkText)}
+        >
+            Toggle Tex Color
+        </button>
+    </form>
+  )
+}
+
+export default Input
