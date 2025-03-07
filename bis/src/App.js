a18ea9e2e82af6cacc9e28f248bf5f7e8b57cc3c
jmabbas <mdabbas.it@gmail.com>
2025-03-07 20:54:53 +0530

    create feature added

--- bis/src/App.js
+++ bis/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import { useState, useEffect } from 'react';
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 function App() {
   const API_URL ='http://localhost:3500/items';
@@ -35,11 +36,22 @@ function App() {
     (async () => fetchItems()) ()
   },[])
 
-  const addItem = (item) => {
+  const addItem = async( item) => {
     const id = items.length ? items[items.length - 1].id+1 : 1;
     const addNewItem = {id, checked: false, item}
     const listItems = [...items, addNewItem]
     setItem(listItems)
+
+    const postOptions = {
+      method:'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(addNewItem)
+    }
+
+    const result =await apiRequest(API_URL,postOptions)
+    if(result) setFetcherror(result)
   }
 
   const handleChange =(id) => {
