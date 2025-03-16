3207be0ebedb78324f15f2498d693a9bcc82ddfd
jmabbas <mdabbas.it@gmail.com>
2025-03-16 19:35:43 +0530

    data featch

--- bis/src/App.js
+++ bis/src/App.js
@@ -1,10 +1,36 @@
 import { useState, useEffect } from 'react';
+import Form from './Form';
+import List from './List';
+import Table from './Table';
 
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+
+  const [reqType, setReqtype] = useState('users');
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+
+        const data = await response.json();
+        setItems(data);
+        console.log (data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchItems();
+    
+  },[reqType])
 
   return (
     <div className="App">
-     test
+      <Form reqType={reqType} setReqtype={setReqtype} />
+      {/* <List items={items} /> */}
+      <Table items={items} />
     </div>
   );
 }
