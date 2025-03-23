4004b787b61155dee360dfd91e5b5c6f7b345ab0
jmabbas <mdabbas.it@gmail.com>
2025-03-23 20:26:09 +0530

    router added

--- bis/src/App.js
+++ bis/src/App.js
@@ -4,19 +4,28 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
 import Newpost from "./Newpost";
+import Postpage from "./Postpage";
+import { Missing } from "./Missing";
+import { PostLayout } from "./PostLayout";
+import Post from "./Post";
 
 function App() {
   return (
     <div className="App">
-      <ul>
+      <ul> 
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/newpost">Newpost</Link></li>
+        <li><Link to="/postpage">Postpage</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />  }/>
         <Route path="/about" element={<About />  }/>
-        <Route path="/newpost" element={<Newpost />}/>
+        <Route path="/postpage" element={<PostLayout />}> 
+          <Route index element={<Postpage/>}/>
+          <Route path=":id" element={<Post/>}/>
+          <Route path="newpost" element={<Newpost />}/>
+        </Route>
+        <Route path="*" element={<Missing />}/> 
       </Routes>
     </div>
   );
