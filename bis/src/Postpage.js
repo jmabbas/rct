7c5264f911ea32eb410fa2d62228fc33573c4063
jmabbas <mdabbas.it@gmail.com>
2025-03-23 20:26:16 +0530

    router added

---
+++ bis/src/Postpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Postpage = () => {
+return (
+    <main>
+        <h1>
+            PostPage
+        </h1>
+        <br /><br />
+    </main>
+  )
+}
+
+export default Postpage
