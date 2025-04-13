6f87d415883a561153d3ede360b7c547a700d243
jmabbas <mdabbas.it@gmail.com>
2025-04-13 10:33:44 +0530

    social axios

--- bis/src/App.js
+++ bis/src/App.js
@@ -1,3 +1,5 @@
+//npx json-server -p 3500 -w data/db.json
+
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import About from "./About";
 import Footer from "./Footer";
@@ -11,39 +13,33 @@ import Post from "./Post";
 import { useEffect, useState } from "react";
 import Nav from "./Nav";
 import {format} from "date-fns"
+import api from "./api/posts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id:1,
-      title: "My first Post",
-      datetime: "July 01, 2021",
-      body :"Made a video for first post"
-    },
-    {
-      id:2,
-      title: "My second Post",
-      datetime: "July 02, 2022",
-      body :"Made a video for second post"
-    },
-    {
-      id:3,
-      title: "My third Post",
-      datetime: "July 03, 2023",
-      body :"Made a video for third post"
-    },
-    {
-      id:4,
-      title: "My fourth Post",
-      datetime: "July 04, 2024",
-      body :"Made a video for fourth post"
-    },
-  ])
+  const [posts, setPosts] = useState([])
   const [search, setSearch] =useState('')
   const [searchResults, setSearchResults]= useState([])
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try{
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error ${err.message}`);
+        }
+      }
+    }
+    fetchPosts();
+  },[])
 
   useEffect(()=> {
     const filterResults = posts.filter((post)=>
